b900536d982b3a9d0f693e265d78ba17ec3c2697
Davis <davis.dai@go6d.com>
2017-05-11 17:54:39 +0800

    add product package

---
+++ src/test/groovy/io/reactivesw/product/CreateProductTest.groovy
@@ -0,0 +1,9 @@
+package io.reactivesw.product
+
+import spock.lang.Specification
+
+/**
+ * Create product test.
+ */
+class CreateProductTest extends Specification{
+}
---
+++ src/test/groovy/io/reactivesw/product/config/ProductConfig.groovy
@@ -0,0 +1,12 @@
+package io.reactivesw.product.config
+
+import io.reactivesw.config.GlobalConfig
+
+/**
+ * Configuration of category service.
+ */
+class ProductConfig {
+    static final String path = "products"
+
+    static final String rootURL = GlobalConfig.host + path + "/"
+}
